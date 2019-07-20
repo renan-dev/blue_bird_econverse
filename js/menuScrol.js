cf465baf8ee227e66fd5703d6955d09c3177a7c7
Renan-dev <renanprch@gmail.com>
2019-07-20 03:02:37 -0300

    finish him

---
+++ js/menuScrol.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector('nav');
+
+window.onscroll = () => {
+    const top = window.pageYOffset;
+    if (top > 150) {
+        menu.classList.add('menu-aux');
+        document.querySelector('.logo img').src = '../img/logo-puro.png';
+    }
+    if (top === 0) {
+        menu.classList.remove('menu-aux');
+    }
+}
